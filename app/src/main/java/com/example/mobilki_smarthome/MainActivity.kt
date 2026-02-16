9508d92f27311546cc8069837767c5fd28a468f1
jakub.artysiewicz_in <jakub.artysiewicz@infotech.edu.pl>
2026-02-16 15:09:00 +0100

    smiga

--- app/src/main/java/com/example/mobilki_smarthome/MainActivity.kt
+++ app/src/main/java/com/example/mobilki_smarthome/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.mobilki_smarthome
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.SeekBar
+import android.widget.Switch
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -19,8 +23,24 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        var numberOfViews = 1
         val sbValue = findViewById<SeekBar>(R.id.sbValue)
         var tvValue = findViewById<TextView>(R.id.tvValue)
+        var btnCommit = findViewById<Button>(R.id.btnCommit)
+        var swHeat = findViewById<Switch>(R.id.swHeat)
+        var tvLogStatus = findViewById<TextView>(R.id.tvLogStatus)
+        var tvLogsHistoryLayout = findViewById<LinearLayout>(R.id.tvLogsHistoryLayout)
+
+        btnCommit.setOnClickListener {
+
+            val newTextView = TextView(this)
+            newTextView.setText("Numer: ${numberOfViews} Status: ${swHeat.isChecked} Moc: ${sbValue.progress}%")
+            tvLogsHistoryLayout.addView(newTextView , 0)
+            tvLogStatus.text = "Pomyślnie dodano wpis nr: ${numberOfViews}"
+            tvLogStatus.setTextColor(Color.parseColor("#008577"))
+
+            numberOfViews+=1
+        }
         sbValue.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(
                 seekBar: SeekBar?,
